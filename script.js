4a019c56004b40ba222864cadb42d7ce8f0fd013
dndus <onenavero@gmail.com>
2022-03-17 12:43:01 +0500

    script

--- script.js
+++ script.js
@@ -60,5 +60,8 @@ window.addEventListener('load', () => {
             }
         });
 
-    })
-})
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el)
+        });
+    });
+});
